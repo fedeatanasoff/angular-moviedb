563a1914a5cab6d343660e5a195470fd500a63aa
fedeatanasoff <fede.atanasoff@gmail.com>
2019-03-02 20:40:51 -0300

    componentes peliculas, home, navbar, buscar creados. configurados en el routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MoviesService } from "./services/movies.service";
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, PeliculaComponent, BuscarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
